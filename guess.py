546e6baa3401fe81ffb01bf145c26cdc4a5dc4c6
shivam raj <shivambedi345@gmail.com>
2025-03-14 18:31:19 +0530

    Delete guess.py
    delete

--- guess.py
+++
@@ -1,6 +0,0 @@
-import random
-low = int(input("enter the lowest range :"))
-high = int(input("enter the highest range :"))
-options = ("Rock", "Paper", "scissors")
-number = random.randint(low, high)
-print(number)
